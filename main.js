6426170b4ef6209652aa6af681f594ff0e1cae9f
kpripper <102256496+kpripper@users.noreply.github.com>
2022-10-18 20:47:54 +0300

    Видалення юзерів

--- main.js
+++ main.js
@@ -1,67 +1,79 @@
 function DataTable(config) {
+  console.log(config)
+  let configStringified = JSON.stringify(config)
+  console.log(configStringified)
+  let configParsed = JSON.parse(configStringified)
+  console.log(configParsed)
   fetch(config.apiUrl)
     .then((response) => {
       return response.json()
     })
     .then((data) => {
       console.log(data)
-      console.log(Object.keys(data))
-      console.log(Object.keys(data.data))      
-      console.log(Object.keys(data.data["1"]))
-      console.log(Object.keys(data.data["1"].name))
-      console.log(Object.keys(data.data["1"]))
 
+      // console.log(Object.keys(data))
+      // console.log(Object.keys(data.data))
+      // console.log(Object.keys(data.data["1"]))
+      // console.log(Object.values(data.data["1"]))
+      // console.log(data.data)
+      // console.log(data.data['1'])
+      // console.log(data.data['1'].name)
+      // console.log(Object.keys(data.data["1"]["name"]))
+      // console.log(Object.values(data.data["1"].name))
       // console.log(Object.keys(data))
       // console.log(Object.values(data))
       // console.log(Object.values(data.data))
-    //  console.log(Object.keys(data.data))
-    // console.log(Object.entries(data))
-    //  console.log(Object.entries(data.data))
-    //   console.log("(data.data)[0]"+Object.keys(data.data)[0])
-
-    //   for (let key in data.data) {
-    //     console.log(key)
-    //   }
-
-    //   for (const [key, value] of Object.entries(data.data)) {
-    //     console.log(`${key}: ${value}`);
-    //   }
-
+      //  console.log(Object.keys(data.data))
+      // console.log(Object.entries(data))
+      //  console.log(Object.entries(data.data))
+      //   console.log("(data.data)[0]"+Object.keys(data.data)[0])
+      //   for (let key in data.data) {
+      //     console.log(key)
+      //   }
+      //   for (const [key, value] of Object.entries(data.data)) {
+      //     console.log(`${key}: ${value}`);
+      //   }
       //  console.log(Object.keys(data.data.1.name);
-      let peopleCount = Object.values(data.data).length
-    //   console.log(data)
-    //   console.log(data.data)
-    //   let dataData = data.data
-    //   console.log("dataData")
-    //   console.log(dataData)
-    //   console.log(dataData.1)
+      // let peopleCount = Object.keys(data.data).length
+      // console.log(peopleCount)
 
-
-      let tableHtml = `<table>
+      let tableHtml = `<table id="tableId">
       <thead>
-          <tr>
-              <th>№</th>
-              <th>${config.columns[0].title}</th>
-              <th>${config.columns[1].title}</th>
-              <th>${config.columns[2].title}</th>
-          </tr>
+        <tr>
+          <th colspan="${Object.keys(config.columns).length + 2}"><button class="add-user" onclick ="addUser()">Add user</button></th>    
+        </tr>      
+        <tr>
+          <th>№</th>
+          <th>${config.columns[0].title}</th>
+          <th>${config.columns[1].title}</th>
+          <th>${config.columns[2].title}</th>
+          <th>${config.columns[3].title}</th>
+          <th>Actions</th>
+        </tr>
       </thead>
       <tbody>`
 
-      for (let i = 0; i < peopleCount.length; i++) {
+      function getYearDiff(date) {
+        return Math.abs(new Date().getFullYear() - new Date(date).getFullYear())
+      }
+
+      for (var key of Object.entries(data.data)) {
+        //  console.log(key)
         tableHtml += `<tr>
-           <td>${i + 1}</td>
-            <td>${data.data[`${i}`].name}</td>
-            <td>${data.data[`${i}`].surname}</td>
-           <td>${data.data[`${i}`].age}</td>
+        <td>${key['0']}</td>
+        <td>${key['1'].name}</td>
+        <td>${key['1'].surname}</td>
+        <td>${getYearDiff(key['1'].birthday)}</td>
+        <td><img src="${key['1'].avatar}"></img></td>
+        <td><button class="button-delete" onclick ="deleteUser(${key['0']}, '${config.apiUrl}')">Delete</button></td>
         </tr>`
       }
 
       tableHtml += `
-      </tbody>
-    </table>`
+        </tbody>
+      </table>`
 
-      console.log(tableHtml)
+      // console.log(tableHtml)
       document.getElementById('usersTable').innerHTML = tableHtml
     })
 }
@@ -72,6 +84,7 @@ const config1 = {
     { title: 'Имя', value: 'name' },
     { title: 'Фамилия', value: 'surname' },
     { title: 'Возраст', value: 'age' },
+    { title: 'Avatar', value: 'avatar' },
   ],
   apiUrl: 'https://mock-api.shpp.me/ccc/users',
 }
@@ -82,9 +95,33 @@ const config2 = {
     { title: 'Name', value: 'name' },
     { title: 'Surname', value: 'surname' },
     { title: 'Age', value: 'age' },
-    //   { title: 'Avatar', value: 'age' },
+    { title: 'Avatar', value: 'avatar' },
   ],
   apiUrl: 'https://mock-api.shpp.me/ipigovych/users',
+}
+
+// function deleteUser(id, url) {
+//   fetch('https://mock-api.shpp.me/ccc/users/' + id, {
+//     method: 'DELETE',
+//   }).then(() => {
+//     console.log(id + 'deleted')
+//     DataTable(config1)
+//   })
+// }
+
+function deleteUser(id, url) {
+  //console.log(config)
+  fetch(url + '/' + id, {
+    method: 'DELETE',
+  }).then(() => {
+    console.log(id + 'deleted')
+    DataTable(config1)
+  })
+}
+
+function addUser() {
+  let table = document.getElementById("tableId");
+  console.log(table)
 }
 
 DataTable(config1)
