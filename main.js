5fb3f16cae2ed447488a50df8a412c002d0d0834
kpripper <102256496+kpripper@users.noreply.github.com>
2022-10-22 18:35:06 +0300

    Рев'ю

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const config1 = {
     { title: 'Avatar', value: 'avatar' },
   ],
   apiUrl: 'https://mock-api.shpp.me/ccc/users',
-}
+};
 
 const config2 = {
   parent: '#usersTable',
@@ -17,20 +17,22 @@ const config2 = {
     { title: 'Age', value: 'birthday' },
     { title: 'Avatar', value: 'avatar' },
   ],
-  apiUrl: 'https://mock-api.shpp.me/ipigovych/users',
-}
+  // apiUrl: 'api.txt',
+  apiUrl: 'https://mock-api.shpp.me/ipihovych/users',
+};
 
-let config = config2
+let config = config2;
 
 //////////make table//////////////
 
 function DataTable(config) {
-  fetch(config.apiUrl)
-    .then((response) => {
-      return response.json()
-    })
-    .then((data) => {
-      let tableHtml = `<table id="tableId">
+  if (config.apiUrl) {
+    fetch(config.apiUrl)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        let tableHtml = `<table id="tableId">
       <thead>
         <tr>
           <th colspan="${
@@ -38,23 +40,25 @@ function DataTable(config) {
           }"><button class="add-user" onclick ="addUser()">Add user</button></th>    
         </tr>      
         <tr>
-          <th>№</th>`
+          <th>№</th>`;
 
-      for (let i = 0; i < Object.entries(config.columns).length; i++) {
-        tableHtml += `<th>${config.columns[i].title}</th>`
-      }
+        for (let i = 0; i < Object.entries(config.columns).length; i++) {
+          tableHtml += `<th>${config.columns[i].title}</th>`;
+        }
 
-      tableHtml += `<th>Actions</th>
+        tableHtml += `<th>Actions</th>
         </tr>
       </thead>
-      <tbody>`
+      <tbody>`;
 
-      function getYearDiff(date) {
-        return Math.abs(new Date().getFullYear() - new Date(date).getFullYear())
-      }
+        function getYearDiff(date) {
+          return Math.abs(
+            new Date().getFullYear() - new Date(date).getFullYear()
+          );
+        }
 
-      for (var key of Object.entries(data.data)) {
-        tableHtml += `<tr>
+        for (var key of Object.entries(data.data)) {
+          tableHtml += `<tr>
         <td>${key['0']}</td>
         <td>${key['1'].name}</td>
         <td>${key['1'].surname}</td>
@@ -63,17 +67,20 @@ function DataTable(config) {
           key['1'].avatar
         }"></img></td>
         <td><button class="button-delete" onclick ="deleteUser(${key['0']}, '${
-          config.apiUrl
-        }')">Delete</button></td>
-        </tr>`
-      }
+            config.apiUrl
+          }')">Delete</button></td>
+        </tr>`;
+        }
 
-      tableHtml += `
+        tableHtml += `
         </tbody>
-      </table>`
+      </table>`;
 
-      document.getElementById('usersTable').innerHTML = tableHtml
-    })
+        document.getElementById('usersTable').innerHTML = tableHtml;
+      });
+  } else {
+    alert('No API url!');
+  }
 }
 
 function deleteUser(id, url) {
@@ -81,47 +88,71 @@ function deleteUser(id, url) {
     method: 'DELETE',
   })
     .then(() => {
-      DataTable(config)
+      DataTable(config);
     })
     .catch((error) => {
-      console.error('Error:', error)
-    })
+      console.error('Error:', error);
+    });
 }
 
 function addUser() {
   ///////////////add new row with inputs//////////////
-  const row = document.createElement('tr')
-  const td = document.createElement('td')
 
-  td.innerHTML = ''
-  row.appendChild(td)
+  if (!document.querySelector('input')) {
+    const row = document.createElement('tr');
+    const td = document.createElement('td');
 
-  for (let i = 0; i < Object.entries(config1.columns).length; i++) {
-    const td = document.createElement('td')
-    const inp = `<div class="input-container"><input class="input" type="text" id="${config.columns[i].value}" name="${config.columns[i].value}" placeholder="${config.columns[i].value}"></div>`
-    td.innerHTML = inp
-    row.appendChild(td)
+    td.innerHTML = '';
+    row.appendChild(td);
+
+    for (let i = 0; i < Object.entries(config1.columns).length; i++) {
+      const tdLoop = document.createElement('td');
+      const inp = `<div class="input-container"><input class="input" type="text" id="${config.columns[i].value}" name="${config.columns[i].value}" placeholder="${config.columns[i].value}"></div>`;
+      tdLoop.innerHTML = inp;
+      row.appendChild(tdLoop);
+    }
+
+    const add = document.createElement('td');
+    const buttonAdd = `<button class="add-button">Add</button>`;
+    add.innerHTML = buttonAdd;
+    row.appendChild(add);
+
+    document
+      .getElementById('tableId')
+      .childNodes[3].insertBefore(
+        row,
+        document.getElementById('tableId').childNodes[3].firstChild
+      );
   }
-
-  const add = document.createElement('td')
-  const buttonAdd = `<button class="add-button">Add</button>`
-  add.innerHTML = buttonAdd
-  row.appendChild(add)
-
-  document
-    .getElementById('tableId')
-    .childNodes[3].insertBefore(
-      row,
-      document.getElementById('tableId').childNodes[3].firstChild
-    )
 
   ////////add user to API/////////////////////
 
-  document.querySelector('.add-button').onclick = function (url) {
-    let name = document.querySelector('input[name=name]').value
-    let surname = document.querySelector('input[name=surname]').value
-    let birthday = document.querySelector('input[name=birthday]').value
-    let avatar = document.querySelector('input[name=avatar]').value
+  let elementsArray = document.querySelectorAll('input');
+
+  elementsArray.forEach(function (elem) {
+    elem.addEventListener('focus', checkInputs);
+    elem.addEventListener('keypress', addingUser);
+  });
+
+  function checkInputs() {
+    elementsArray.forEach(function (elem) {
+      if (elem.value === '') {
+        elem.classList.add('red-border');
+      } else {
+        elem.classList.remove('red-border');
+      }
+    });
+  }
+
+  document.querySelector('.add-button').addEventListener('click', addingUser);
+
+  function addingUser() {
+    checkInputs();
+
+    let name = document.querySelector('input[name=name]').value;
+    let surname = document.querySelector('input[name=surname]').value;
+    let birthday = document.querySelector('input[name=birthday]').value;
+    let avatar = document.querySelector('input[name=avatar]').value;
 
     if (name && surname && birthday && avatar) {
       fetch(config.apiUrl, {
@@ -134,25 +165,13 @@ function addUser() {
         }),
       })
         .then((data) => {
-          alert('Added')
-          console.log(data)
-          DataTable(config)
+          alert('Added');
+          console.log(data);
+          DataTable(config);
         })
         .catch((error) => {
-          console.error('Error:', error)
-        })
-    } else {
-      for (let i = 0; i < Object.entries(config1.columns).length; i++) {
-        checkInput(config.columns[i].value)
-      }
-    }
-  }
-
-  function checkInput(inp) {
-    if (document.querySelector(`input[name=${inp}]`).value === '') {
-      document.querySelector(`input[name=${inp}]`).classList.add('red-border')
-    } else {
-      console.log('value ' + document.querySelector(`input[name=${inp}]`).value)
+          console.error('Error:', error);
+        });
     }
   }
 }
@@ -179,4 +198,4 @@ function addUser() {
 
 ///////run script//////////////////////
 
-DataTable(config)
+DataTable(config);
